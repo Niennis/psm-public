8c6958cb10ab8a0b108991f769004dccc1aa5743
cbustoso <121638773+cbustoso@users.noreply.github.com>
2024-10-01 20:33:41 -0300

    Update site public

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='src/app')
 
 @app.route('/')
 def index():
-    return app.send_static_file('page.jsx')  # Asegúrate de tener este archivo en la carpeta public
+    return app.send_static_file('index.html')  # Asegúrate de tener este archivo en la carpeta public
 
 @app.route('/api/data')
 def get_data():
@@ -13,3 +13,4 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+    
