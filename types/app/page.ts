cda849f8af2612ba2418e813c8d3ebd159c36dae
Cristóbal Bustos <cbustos@zerus.cl>
2024-10-15 21:56:48 +0000

    Updated page.ts

--- types/app/page.ts
+++ types/app/page.ts
@@ -2,6 +2,7 @@
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
+
 type TEntry = typeof import('../../../src/app/page.js')
 
 // Check that the entry is a valid entry
