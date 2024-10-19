58fc4a8d1ba9e8b4614485bd3967ff68702cf425
Estefanía Osses <eosses@zerus.cl>
2024-10-19 02:11:48 +0000

    Updated server.js cambiar configuración del puerto

--- src/app/api/server.js
+++ src/app/api/server.js
@@ -1,20 +1,4 @@
-const express = require('express');
-const next = require('next');
-
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  const server = express();
-  const port = process.env.PORT || 3000;
-
-  server.get('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
